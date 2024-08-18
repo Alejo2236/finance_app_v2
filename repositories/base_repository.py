7d149f779a31147614f6453e50d48801c85eec63
Alejo2236 <alejo.benitez2236@gmail.com>
2024-08-18 16:03:55 -0500

    Docstrings added to base repository and category repository

--- repositories/base_repository.py
+++ repositories/base_repository.py
@@ -7,36 +7,98 @@ T = TypeVar('T')
 
 
 class BaseRepository(ABC, Generic[T]):
+    """
+    Base class for generic repository operations.
+
+    This class provides basic CRUD (Create, Read, Update, Delete) operations
+    for any SQLAlchemy model class.
+
+    :param session: SQLAlchemy session used for database operations.
+    :type session: Session
+    :param model_class: Model class associated with the repository.
+    :type model_class: Type[T]
+    """
+
     def __init__(self, session: Session, model_class: Type[T]):
-        self.__session = session
-        self.__model_class = model_class
+        """
+        Initializes the repository with a database session and model class.
+
+        :param session: SQLAlchemy session used to interact with the database.
+        :type session: Session
+        :param model_class: The SQLAlchemy model class this repository manages.
+        :type model_class: Type[T]
+        """
+
+        self._session = session
+        self._model_class = model_class
 
     def add(self, instance: T) -> None:
-        self.__session.add(instance)
+        """
+        Adds an instance of the model to the session.
+
+        :param instance: The model instance to add to the session.
+        :type instance: T
+        """
+
+        self._session.add(instance)
 
     def get_by_primary_key(self, primary_key) -> Optional[T]:
-        return self.__session.query(self.__model_class).get(primary_key)
+        """
+        Retrieves an instance of the model by its primary key.
+
+        :param primary_key: The primary key of the model instance to retrieve.
+        :type primary_key: Any
+        :returns: The instance of the model, or None if not found.
+        :rtype: Optional[T]
+        """
+
+        return self._session.query(self._model_class).get(primary_key)
 
     def list(self) -> list[T]:
-        return self.__session.query(self.__model_class).all()
+        """
+        Retrieves all instances of the model from the database.
+
+        :returns: A list of all model instances.
+        :rtype: list[T]
+        """
+
+        return self._session.query(self._model_class).all()
 
     def update_by_primary_key(self, primary_key, **kwargs) -> None:
-        instance = self.__session.query(self.__model_class).get(primary_key)
+        """
+        Updates an instance of the model by its primary key with provided attributes.
+
+        :param primary_key: The primary key of the model instance to update.
+        :type primary_key: Any
+        :param kwargs: Key-value pairs of attributes to update on the model instance.
+        :raises ValueError: If the model instance with the given primary key is not found.
+        :raises AttributeError: If an invalid attribute is provided for the model.
+        """
+
+        instance = self._session.query(self._model_class).get(primary_key)
 
         if not instance:
-            raise ValueError(f'Object "{self.__model_class.__name__}" with primary key "{primary_key}" not found.')
+            raise ValueError(f'Object "{self._model_class.__name__}" with primary key "{primary_key}" not found.')
 
         for key, value in kwargs.items():
             if not hasattr(instance, key):
-                raise AttributeError(f'{self.__model_class.__name__} object has no attribute {key}.')
+                raise AttributeError(f'{self._model_class.__name__} object has no attribute {key}.')
             setattr(instance, key, value)
 
-        self.__session.add(instance)
+        self._session.add(instance)
 
     def delete_by_primary_key(self, primary_key) -> None:
-        instance = self.__session.query(self.__model_class).get(primary_key)
+        """
+        Deletes an instance of the model by its primary key.
+
+        :param primary_key: The primary key of the model instance to delete.
+        :type primary_key: Any
+        :raises ValueError: If the model instance with the given primary key is not found.
+        """
+
+        instance = self._session.query(self._model_class).get(primary_key)
 
         if not instance:
-            raise ValueError(f'Object "{self.__model_class}" with primary key "{primary_key}" not found.')
+            raise ValueError(f'Object "{self._model_class}" with primary key "{primary_key}" not found.')
 
-        self.__session.delete(instance)
+        self._session.delete(instance)
--- repositories/category_repository.py
+++ repositories/category_repository.py
@@ -5,5 +5,18 @@ from repositories.base_repository import BaseRepository
 
 
 class CategoryRepository(BaseRepository[CategoryDb]):
+    """
+    Repository for handling operations related to the Category model.
+
+    Inherits the base CRUD operations from BaseRepository.
+    """
+
     def __init__(self, session: Session):
+        """
+        Initializes the CategoryRepository with a specific session.
+
+        :param session: SQLAlchemy session used to interact with the database.
+        :type session: Session
+        """
+
         super().__init__(session, CategoryDb)
