990afa95d025497f13d1c8c96130866f21883c6e
Alejo2236 <alejo.benitez2236@gmail.com>
2024-08-18 19:08:51 -0500

    creation of category controller, the tests folder and updatind some documentation.

--- controllers/category_controller.py
+++ controllers/category_controller.py
@@ -1,15 +1,42 @@
-from exceptions.object_not_found_error import ObjectNotFoundError
 from models.category import Category
 from services.category_service import CategoryService
 
 
 class CategoryController:
+    """
+    Controller class for managing interactions related to categories.
+
+    This controller serves as an intermediary between the user interface (UI)
+    and the service layer, facilitating user requests and managing responses,
+    including error handling.
+
+    :param category_service: The service used to manage category-related operations.
+    :type category_service: CategoryService
+    """
+
     def __init__(self, category_service: CategoryService):
+        """
+        Initializes the CategoryController with a CategoryService instance.
+
+        :param category_service: The service used to perform category operations.
+        :type category_service: CategoryService
+        """
+
         self.__category_service = CategoryService
 
     def get_category_by_name(self, name: str) -> Category:
-        try:
-            return self.__category_service.get_category_by_name(name)
-        except ObjectNotFoundError:
-            # TODO create a dialog or show in the view the error
-            print("there was an error")
+        """
+        Retrieves a category by its name.
+
+        This method calls the CategoryService to fetch a category based on the
+        provided name. If the category is not found, an ´ObjectNotFoundError´
+        will be propagated to the view for handling.
+
+        :param name: The name of the category to retrieve.
+        :type name: str
+        :return: The category business object if found.
+        :rtype: Category
+        :raises ObjectNotFoundError: If the category with the given name is not found.
+        """
+
+        return self.__category_service.get_category_by_name(name)
--- services/category_service.py
+++ services/category_service.py
@@ -11,9 +11,17 @@ class CategoryService:
     consistency in transactions.
 
     :param unit_of_work: The UnitOfWork instance used to manage database operations.
+    :type unit_of_work: UnitOfWork
     """
 
     def __init__(self, unit_of_work: UnitOfWork):
+        """
+        Initializes the CategoryService with a UnitOfWork instance.
+
+        :param unit_of_work: The UnitOfWork instance used for managing database operations.
+        :type unit_of_work: UnitOfWork
+        """
+
         self.__unit_of_work = unit_of_work
 
     def get_category_by_name(self, name: str) -> Category:
@@ -21,13 +29,17 @@ class CategoryService:
         Retrieves a category by its name.
 
         This method uses the Unit of Work to fetch the category from
-        the repository within a transactional context. If any error occurs in the repository, the expection will be propagated
+        the repository within a transactional context. If the category is
+        not found, the `ObjectNotFoundError` is raised by the repository and
+        propagated up to this method.
 
         :param name: The name of the category to retrieve.
         :type name: str
         :return: The category business object if found.
         :rtype: Category
+        :raises ObjectNotFoundError: If the category with the given name is not found in the repository.
         """
+
         with self.__unit_of_work as uow:
             category_repository = uow.categories
             category_database_model = category_repository.get_by_primary_key(name)
